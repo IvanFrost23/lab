8967bbaa9d9aa8a3052a1355aae3ed1c4fc70617
Ivan Morozov <morozov.medo@gmail.com>
2025-03-01 19:03:19 +0500

    fix

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,9 +5,7 @@ from .models import CustomUser
 
 class RegistrationForm(UserCreationForm):
     email = forms.EmailField(required=True)
-    first_name = forms.CharField(label='Имя', required=True)
-    last_name = forms.CharField(label='Фамилия', required=False)
 
     class Meta:
         model = CustomUser
-        fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2')
+        fields = ('username', 'email', 'password1', 'password2')
